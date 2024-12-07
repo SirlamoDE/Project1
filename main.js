9351af10b116ff78b7f86cd57de8d5861a56d9e1
SirlamoDE <ollysurfaprod@gmail.com>
2024-12-07 16:46:47 +0100

    ADDING FILES

---
+++ main.js
@@ -0,0 +1,70 @@
+//External Javascript
+
+console.log("THE BEGINNING OF JAVASCRIPT DAY 2(07/12/2024)");
+
+console.log("Message");
+
+// Variables  store data that can be used or manipulated in a programming
+
+//Declaring variables in Javascript
+//Variables are declared using the let and const keyword followed by the variable name.
+
+let book = "Dune"
+
+console.log(book);
+
+book = "42 LAWs OF POWER"
+
+console.log(book)
+
+
+// Using const keyword
+
+const name = "James";
+console.log(name);
+
+// name=John;
+// console.log(name);
+
+//we use camelCase when naming our variables with more than one word
+
+let myFirstName = "Meshach";
+    
+console.log(myFirstName);
+
+myFirstName = "James";
+console.log(myFirstName);
+
+
+const lastName = "Adedotun";
+console.log(lastName);
+
+
+let user = "Peter";
+// console.log(user);
+
+console.log(`Hello, ${user}!`);
+
+let food = "Rice";
+console.log(`my name is ${user},I love to eat ${food}!` );
+
+console.log("Creating a simple calculator that takes two variables and carry four oeprations on them");
+
+let a = 5; b = 7;
+if (a > b){
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a/b);
+}else console.log(`${a} is less than ${b}!`);
+
+a = 8;
+if (a > b){
+    console.log(a+b);
+    console.log(a-b);
+    console.log(a/b);
+    console.log(a*b);
+
+}else console.log(`${a} is less than ${b}!`);
+
+
+console.log("END OF JAVASCRIPT DAY 2(07/12/2024)");
